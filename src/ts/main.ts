75c52ffd45ebe56d715248a0bd70cf7df2089d49
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-02-13 20:45:17 +0100

    card animation should work on click event

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -13,4 +13,31 @@ menu?.addEventListener("click", ()=> {
     menuContainer?.classList.toggle("hide");
 })
 
+// CARD ANIMATION
+
+const cardAbout = document.getElementById("card-about");
+cardAbout?.addEventListener("click", ()=> {
+    cardAbout.classList.toggle("cards-container__about--hide");
+    cardAbout.classList.toggle("cards-container__about--show");
+})
+
+const cardSkill = document.getElementById("card-skills");
+cardSkill?.addEventListener("click", ()=> {
+    cardSkill.classList.toggle("cards-container__about--hide");
+    cardSkill.classList.toggle("cards-container__about--show");
+})
+
+const cardsProject = document.getElementsByClassName(
+    'card-project') as HTMLCollectionOf<HTMLDivElement>;
+for (let i = 0; i < cardsProject.length; i++) {
+    cardsProject[i].addEventListener("click", () => {
+        cardsProject[i].classList.toggle("cards-container__about--hide");
+        cardsProject[i].classList.toggle("cards-container__about--show");
+    })
+}
+// for (let i = 0; i < cardsProject.length; i++) {
+//     cardsProject[i]?.addEventListener("click", ()=> {
+//         cardsProject[i].classList.toggle("cards-container__about--hide");
+//         cardProject.classList.toggle("cards-container__about--show");
+//     })
 
