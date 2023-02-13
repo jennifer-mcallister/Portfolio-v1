09aab5c1b1d3d6b7a5c92c093bbfe6c5838b9a01
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-02-13 18:40:41 +0100

    added dividers and card styling

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,8 +3,14 @@
 const menu = document.getElementById("hamburger-menu");
 menu?.classList.add("unToggled");
 
+const menuContainer = document.getElementById("menu-options");
+menuContainer?.classList.add("hide");
+
 menu?.addEventListener("click", ()=> {
     menu.classList.toggle("toggled");
     menu.classList.toggle("unToggled");
+    menuContainer?.classList.toggle("show");
+    menuContainer?.classList.toggle("hide");
 })
+
 
